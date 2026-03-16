ca70c6bd5ff24a455d167d75e1977663b608b8ff
medatejakshay-maker <medatejakshay@gmail.com>
2026-03-16 10:15:22 +0530

    YourName Added UC2-HardCodePalindrome

---
+++ Desktop/RA2411003012395/BookMyStay/BookMyStay/src/App.java
@@ -0,0 +1,15 @@
+public class App {
+
+    public static void main(String[] args) {
+
+        // Display welcome message
+        System.out.println("Welcome to the Hotel Booking System!");
+
+        // Display application name and version
+        System.out.println("Application: Hotel Booking System");
+        System.out.println("Version: 1.0");
+
+        // End message
+        System.out.println("Application started successfully.");
+    }
+}
---
+++ Documents/pallindromecheckerApp/App/src/pallindromecheckerApp.java
@@ -0,0 +1,33 @@
+/**
+ MAIN CLASS UseCase PalindromeApp
+ Use Case 1: Application Entry & Welcome Message
+ Description:
+ This class represents the entry point of the
+ Palindrome Checker Management System.
+ At this stage, the application:
+ Starts execution from the main() method
+ Displays a welcome message
+ Shows application version
+ No palindrome logic is implemented yet.
+ The goal is to establish a clear startup flow.
+ @author Ramya Anandan
+ @version 1.0
+ **/
+
+
+
+
+
+public class pallindromecheckerApp {
+/**
+ Application entry point.
+ This is the first method executed by the JVM
+ when the program starts.
+ @param args command-Line arguments
+**/
+    public static void main() (String[] args){
+        System.out.println("Welcome to pallindrome checker management system");
+        System.out.println("version 1.0");
+        System.out.println("System initialized Successfully.");
+    }
+}
